49c5b937bb22d1109624c08c727886ffcdaf1a7b
robonagersteam <ungureanusebastian067@gmail.com>
2025-11-17 14:42:19 +0200

    upgraded to previous
    fixed gradle build now there is other promblem with main activity

--- teamcode/src/main/java/com/example/teamcode/MainActivity2.java
+++ teamcode/src/main/java/com/example/teamcode/MainActivity2.java
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.firstinspires.ftc.teamcode.R;
+
 public class MainActivity2 extends AppCompatActivity {
 
     @Override
--- teamcode/src/main/java/org/firstinspires/ftc/teamcode/GamePadPractice.java
+++ teamcode/src/main/java/org/firstinspires/ftc/teamcode/GamePadPractice.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @Disabled
-@TeleOp(GamePadPractice)
+@TeleOp()
 public class GamePadPractice extends OpMode {
 
     @Override
